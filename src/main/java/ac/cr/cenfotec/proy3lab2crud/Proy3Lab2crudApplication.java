d763fa99dd88242101ed61ef90c9e4030b53d5ef
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2022-06-07 19:18:35 -0600

    commit de la base del proyecto

---
+++ src/main/java/ac/cr/cenfotec/proy3lab2crud/Proy3Lab2crudApplication.java
@@ -0,0 +1,13 @@
+package ac.cr.cenfotec.proy3lab2crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proy3Lab2crudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proy3Lab2crudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/cr/cenfotec/proy3lab2crud/Proy3Lab2crudApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.cr.cenfotec.proy3lab2crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proy3Lab2crudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
